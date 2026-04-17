c427fc8c41e992854cf98039a3ba96151523db92
Shubham Chede <143908945+ShubhamSChede@users.noreply.github.com>
2026-04-17 15:38:02 +0530

    Implement student record management menu
    Added a menu for managing student records with options to add regular and scholarship students, display all records, and exit.

--- main.cpp
+++ main.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int choice;
+
+    while (true) {
+        cout << "1. Add Regular Student\n";
+        cout << "2. Add Scholarship Student\n";
+        cout << "3. Display All Records\n";
+        cout << "4. Exit\n";
+
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "Adding Regular Student\n";
+            case 2:
+                cout << "Adding Scholarship Student\n";
+            case 3:
+                cout << "Displaying Records\n";
+            case 4:
+                cout << "Exiting...\n";
+                return 0;
+            default:
+                cout << "Invalid choice\n";
+        }
+    }
+}
+
 
